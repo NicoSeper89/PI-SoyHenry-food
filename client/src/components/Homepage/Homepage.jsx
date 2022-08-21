348527df6743fe249e7a6b7833cf86386465b01c
nicoseper89 <nicosepertino@hotmail.com>
2022-08-21 00:40:08 -0300

    v1.42

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -5,9 +5,13 @@ import { NavLink } from 'react-router-dom';
 
 export default function Homepage() {
     return (
-        <div className={style.homepageContainers} >
+        <div className={style.background} >
             <NavLink to="/">
-                <button>INGRESAR</button>
+                <div className={style.lineWhite}>
+                    <img src="" alt="" />
+                    <button>INGRESAR</button>
+                    <div><a href="https://www.google.com">GIT</a><a href="www.google.com">LINKEDIN</a></div>
+                </div>
             </NavLink>
         </div>
     )
