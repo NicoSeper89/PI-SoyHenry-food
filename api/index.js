6d70dc11823626dadfb6473c1aaf5f139450e82c
nicoseper89 <nicosepertino@hotmail.com>
2023-03-11 01:10:21 -0300

    vercel deploy

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ const typesOfDiets = ["dairy free",
 conn.sync({ force: false })
     .then(() => {
       server.listen(process.env.PORT, () => {
-          console.log('%s listening at 3001'); // eslint-disable-line no-console
+          console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
           typesOfDiets.map((type) => Diet.findOrCreate({where: {name: type}}))
         });
     });
--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,15 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE } = process.env;
 let sequelize = process.env.NODE_ENV === 'production'
   ? new Sequelize({
-    database: DB_NAME,
-    dialect: "postgres",
-    host: DB_HOST,
-    port: 5432,
-    username: DB_USER,
-    password: DB_PASSWORD,
+    database: PGDATABASE,
+    dialect: "postgres-neon",
+    host: PGHOST,
+    port: 5433,
+    username: PGUSER,
+    password: PGPASSWORD,
     pool: {
       max: 3,
       min: 1,
@@ -27,7 +27,7 @@ let sequelize = process.env.NODE_ENV === 'production'
     ssl: true,
   })
   : new Sequelize(
-      `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+      `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}`,
       { logging: false, native: false }
     );
 
